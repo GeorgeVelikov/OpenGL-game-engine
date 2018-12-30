027a1578293bdfd0f4973d8a55c47ead59677eaf
Georgi V. Velikov <georgi.velikov.16@aberdeen.ac.uk>
2018-12-30 14:19:40 +0000

    -finish basic raycasting and basic shading

--- Raycasting experiment/Definitions.h
+++ Raycasting experiment/Definitions.h
@@ -1,18 +1,22 @@
 #define PI 3.1415927
 
-#define SCREEN_WIDTH 120
-#define SCREEN_HEIGHT 40
+// might want to change width and height to your prefferenced window size as rendering is not dynamic just yet
+#define SCREEN_WIDTH 150
+#define SCREEN_HEIGHT 50
 #define SCREEN_PIXEL_COUNT (SCREEN_WIDTH*SCREEN_HEIGHT)
 #define SCREEN_DRAW_START {0,0}
 
-#define FOV (PI/3.0)
-#define MAX_DEPTH 16
-#define ROTATIONAL_STEP (PI/2.)
+#define FOV (PI/3.f)
+#define MAX_DEPTH 16.0f
+#define RAY_STEP 0.1f
+#define ROTATIONAL_STEP (PI/2.f)
 #define MOVEMENT_STEP 5.f
 
-#define MAP_HEIGHT 16
-#define MAP_WIDTH 16.0f
+#define MAP_HEIGHT 16.0f
+#define MAP_WIDTH 32.0f
 
+#define WALL_BOUNDARY_THRESHOLD 0.005f
+#define WALL_BOUNDARY_AMOUNT 2
 #define WALL_LIGHT_SHADE 0x2591
 #define WALL_MEDIUM_SHADE 0x2592
 #define WALL_DARK_SHADE 0x2593
