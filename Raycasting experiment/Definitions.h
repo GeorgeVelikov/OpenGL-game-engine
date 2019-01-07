c31616833c5d0099ca33f82ff47dff661f683544
Georgi V. Velikov <georgi.velikov.16@aberdeen.ac.uk>
2019-01-07 19:29:41 +0000

    -fix release crash
    crash on forward/backward during release is no more

--- Raycasting experiment/Definitions.h
+++ Raycasting experiment/Definitions.h
@@ -1,8 +1,6 @@
-
 #include <chrono>
 #include <vector>
 #include <algorithm>
-
 #include <math.h>
 #include <Windows.h>
 
@@ -14,38 +12,46 @@
 #define SCREEN_PIXEL_COUNT (SCREEN_WIDTH*SCREEN_HEIGHT)
 #define SCREEN_DRAW_START {0,0}
 
+// raycasting
 #define FOV (PI/3.f)
-#define MAX_DEPTH 16.0f
-#define RAY_STEP 0.1f
+#define MAX_DEPTH 16.f
+#define RAY_STEP .1f
+
+// wall specific render settings
+#define WALL_BOUNDARY_THRESHOLD .005f
+#define WALL_BOUNDARY_AMOUNT 2
+
+// movement
 #define ROTATIONAL_STEP (PI/2.f)
 #define MOVEMENT_STEP 5.f
 
-#define MAP_HEIGHT 16.0f
-#define MAP_WIDTH 32.0f
+// map defs should go hand in hand with actual map size for 1:1 accuracy
+#define MAP_HEIGHT 16.f
+#define MAP_WIDTH 32.f
 
-#define WALL_BOUNDARY_THRESHOLD 0.005f
-#define WALL_BOUNDARY_AMOUNT 2
-#define WALL_LIGHT_SHADE 0x2591
-#define WALL_MEDIUM_SHADE 0x2592
-#define WALL_DARK_SHADE 0x2593
-#define WALL_BLACK_SHADE 0x2588
-
+// shader
+#define WALL_SHADE_LIGHT 0x2591
+#define WALL_SHADE_MEDIUM 0x2592
+#define WALL_SHADE_DARK 0x2593
+#define WALL_SHADE_BLACK 0x2588
 #define FLOOR_SHADE 0x002A
 #define CLEAR_SHADE 0x0020
 
-// player state
+// player
 float playerX = 1.5f;
 float playerY = 3.24f;
-float playerYaw = 0.0f;
+float playerYaw = .0f;
 bool playerTouchWall = false;
 
-// game state
+// game
 bool running = true;
 auto timeStamp = std::chrono::system_clock::now();
 auto timeStampDynamic = std::chrono::system_clock::now();
+std::chrono::duration<float> frameTime;
 float fpsNormalise;
 
 // raycasting
+float rayAngle;
 float distanceToWall;
 bool blockBoundary;
 float eyeX;
--- Raycasting experiment/Functions.h
+++ Raycasting experiment/Functions.h
@@ -1,9 +1,9 @@
-void move(float direction){
-    playerX += direction * float(sinf(playerYaw) * MOVEMENT_STEP * fpsNormalise);
-    playerY += direction * float(cosf(playerYaw) * MOVEMENT_STEP * fpsNormalise);
-    if (map[int(playerY) * MAP_WIDTH + int(playerX)] == '#') {
-        playerX -= direction * float(sinf(playerYaw) * MOVEMENT_STEP * fpsNormalise);
-        playerY -= direction * float(cosf(playerYaw) * MOVEMENT_STEP * fpsNormalise);
+void move(float direction) {
+    float x_move = direction * float(sinf(playerYaw) * MOVEMENT_STEP * fpsNormalise);
+    float y_move = direction * float(cosf(playerYaw) * MOVEMENT_STEP * fpsNormalise);
+
+    if (map[int(playerY + y_move) * MAP_WIDTH + int(playerX + x_move)] != '#') {
+        playerX += x_move; playerY += y_move;
     }
 }
 
