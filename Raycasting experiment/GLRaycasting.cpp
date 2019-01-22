8cac8c9b5813038c5d592509ba152800a868061a
Georgi V. Velikov <georgi.velikov.16@aberdeen.ac.uk>
2019-01-22 20:34:09 +0000

    Add transformations

--- Raycasting experiment/GLRaycasting.cpp
+++ Raycasting experiment/GLRaycasting.cpp
@@ -1,6 +1,5 @@
 #include "stdafx.h"
 #include <iostream>  
-
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
@@ -9,14 +8,12 @@
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "headers/stb_image.h"
-
 #include "headers/GLFunctions.h"
 #include "headers/GLShader.h"
 
-
-
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
+
 
 int main()
 {
@@ -26,19 +23,17 @@ int main()
     glfwSetFramebufferSizeCallback(window, frameBufferCheck);
 
     // load all OpenGL function pointers for glad
-    setupGLAD(); 
+    setupGLAD();
 
     // build and compile our shader program
     Shader glShader("shaders/shader.vs", "shaders/shader.fs");
-    unsigned int tex1;
-
 
     float vertices[] = {
-        // first triangle
-        -.5f, -.5f, .0f, 0.f, 0.f, .0f, 0.f, 0.f,
-         .5f, -.5f, .0f, .0f, 1.f, .0f, 1.f, .0f,
-        -.5f, .5f, .0f,  .0f, .0f, 1.f, .0f, 1.f,
-         .5f, .5f, .0f,  1.f, .0f, .0f, 1.f, 1.f
+        // positions      //texture coords
+        -.5f, -.5f, .0f,  0.f, 0.f,
+         .5f, -.5f, .0f,  1.f, 0.f,
+        -.5f,  .5f, .0f,  0.f, 1.f,
+         .5f,  .5f, .0f,  1.f, 1.f
     };
 
     unsigned int indices[] = {
@@ -63,22 +58,18 @@ int main()
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     // set vertex attribute pointers
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), 0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), 0);
     glEnableVertexAttribArray(0);
-    
-    // set colour attribute pointers
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
 
     // texture attributes
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-    glEnableVertexAttribArray(2);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     unsigned int textureTile;
     load2DImageAsTexture(&textureTile, 1, "textures/tile.png");
 
     glShader.use();
-    glUniform1i(glGetUniformLocation(glShader.id, "texture1"), 0);
+    glShader.setInt("texture1", 0);
 
     glPolygonMode(GL_FRONT_AND_BACK, GL_FILL); // GL_LINE//GL_FILL solids
     // render loop    
@@ -91,18 +82,32 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        // bind textures on tex units
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, textureTile);
 
-        // render shape
+        // create transformations
+        glm::mat4 transform = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+        transform = glm::translate(transform, glm::vec3(.2f, -.2f, .0f));
+        transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3(.7f, -.9f, .5f));
+
+        // get marix uniform loc
         glShader.use();
+        unsigned int transformLoc = glGetUniformLocation(glShader.id, "transform");
+        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
+
+        // render shape
         glBindVertexArray(VAO); 
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
     
+        // swap buffers and poll for io events
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
-    // glfw: terminate, clearing all previously allocated GLFW resources.
+    // glfw: terminate and clearing all previously allocated resources.
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     glfwTerminate();
     return 0;
 }
